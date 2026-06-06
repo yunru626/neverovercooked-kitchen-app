bbb8220c4932c6506c82f1d690f8e7d65efc60e0
yunru626 <yunru626@gmail.com>
2026-06-06 17:37:18 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bhlc-v6';
+const CACHE = 'bhlc-v7';
 const ASSETS = ['/', '/index.html', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
